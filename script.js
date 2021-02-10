c60329f2b0eba9b578706f31f4cebd4e28554bec
Sushen Biswas <sushenbiswasaga@gmail.com>
2021-02-10 10:16:22 +0600

    Visibal With Html

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-
 function btcPrice(currentPrice,profitPercentige) {
 
-  var priceWithProfit = ((currentPrice * profitPercentige)/ 100) + currentPrice;
+    var priceWithProfit = ((currentPrice * profitPercentige)/ 100) + currentPrice;
 
-  return priceWithProfit;
+    return priceWithProfit;
 
 }
 
-console.log(btcPrice(37855,-6.5));
+data = btcPrice(173,8);
+
+console.log(data);
+
+document.body.innerHTML = " <p> Price will be: </p>" + " <h1> " + data + "</h1>";

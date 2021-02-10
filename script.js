441f0c32222beaf8fc5ade403a0f5b3e98f4e08a
Sushen Biswas <sushenbiswasaga@gmail.com>
2021-02-10 10:33:22 +0600

    Cripto Price Comment

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// var Bitcoin = 45756;
+// var Ethereum = 1771;
+// var Cardano = 0.7617;
+// var XRP = 0.4732;
+// var Monero = 170;
+// var Dai = 1;
+// var Dash = 134;
+// var EthereumClassic = 10;
+// var Zcash = 110;
+// var SingularityNET = 0.1391;
+// var Autonio = 0.1885;
+
+
+
 function btcPrice(currentPrice,profitPercentige) {
 
     var priceWithProfit = ((currentPrice * profitPercentige)/ 100) + currentPrice;

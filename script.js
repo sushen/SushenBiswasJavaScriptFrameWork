9a0e4e27256a79b01c9f5a692fcbfa7971e7bc8a
Sushen Biswas <sushenbiswasaga@gmail.com>
2021-02-13 12:46:49 +0600

    Object

--- script.js
+++ script.js
@@ -1,27 +1,13 @@
-// var Bitcoin = 45756;
-// var Ethereum = 1771;
-// var Cardano = 0.7617;
-// var XRP = 0.4732;
-// var Monero = 170;
-// var Dai = 1;
-// var Dash = 134;
-// var EthereumClassic = 10;
-// var Zcash = 110;
-// var SingularityNET = 0.1391;
-// var Autonio = 0.1885;
-
-
-
-function btcPrice(currentPrice,profitPercentige) {
-
-    var priceWithProfit = ((currentPrice * profitPercentige)/ 100) + currentPrice;
-
-    return priceWithProfit;
-
+function CriptoCurrency(name,price,marketCap,volume,circulatingSupply,date) {
+    this.name = name;
+    this.price = price;
+    this.marketCap = marketCap;
+    this.volume = volume;
+    this.circulatingSupply = circulatingSupply;
+    this.date = date;
+    
 }
 
-data = btcPrice(173,8);
+var bitcoin = new CriptoCurrency("bitcoin",47000,888513591184,72766434194,18627337,new Date());
 
-console.log(data);
-
-document.body.innerHTML = " <p> Price will be: </p>" + " <h1> " + data + "</h1>";
+console.log(bitcoin);

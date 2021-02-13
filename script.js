3b1f4be1f4e0de6808ace28e291373a2c0cc4cad
Sushen Biswas <sushenbiswasaga@gmail.com>
2021-02-13 13:05:07 +0600

    Object Array in Html

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ function CriptoCurrency(name,price,marketCap,volume,circulatingSupply,date) {
     
 }
 
-var bitcoin = new CriptoCurrency("bitcoin",47000,888513591184,72766434194,18627337,new Date());
+var currencysArray = [
+    new CriptoCurrency("bitcoin",47000,888513591184,72766434194,18627337,new Date()),
+    new CriptoCurrency("ethereum",1834,122226149106,38085385945,114650284,new Date())
 
-console.log(bitcoin);
+];
+
+
+console.log(currencysArray[0].marketCap)
+
+document.body.innerHTML = currencysArray[0].marketCap;
